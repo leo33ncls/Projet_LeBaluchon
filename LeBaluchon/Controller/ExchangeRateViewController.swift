11300390f1ae17e43970c33d33294dae099d1dd2
Léo Nicolas <leoncls3398@gmail.com>
2019-10-22 21:00:02 +0200

    Mise en place de l'interface

--- LeBaluchon/Controller/ExchangeRateViewController.swift
+++ LeBaluchon/Controller/ExchangeRateViewController.swift
@@ -9,11 +9,43 @@
 import UIKit
 
 class ExchangeRateViewController: UIViewController {
-
+    @IBOutlet weak var amountTextField: UITextField!
+    @IBOutlet weak var convertedAmountLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     @IBAction func getExchange(_ sender: UIButton) {
+        let amountToConvert = amountTextField.text
+        
+        if let amountTC = amountToConvert {
+            if let amount = Double(amountTC) {
+                let convertedAmount = amount * 1.2
+                convertedAmountLabel.text = "\(convertedAmount) $"
+            } else {
+                showAlert(message: "Entrez un chiffre!")
+            }
+        } else {
+            showAlert(message: "Veuillez entrer un montant!")
+        }
+    }
+    
+    private func showAlert(message: String) {
+        let alertVC = UIAlertController(title: "Attention!", message: message, preferredStyle: .alert)
+        alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+        return self.present(alertVC, animated: true, completion: nil)
+    }
+}
+
+// MARK: - Keyboard
+extension ExchangeRateViewController: UITextFieldDelegate {
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        amountTextField.resignFirstResponder()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 }
---
+++ LeBaluchon/Controller/TranslationViewController.swift
@@ -0,0 +1,49 @@
+//
+//  TranslationViewController.swift
+//  LeBaluchon
+//
+//  Created by Léo NICOLAS on 22/10/2019.
+//  Copyright © 2019 Léo NICOLAS. All rights reserved.
+//
+
+import UIKit
+
+class TranslationViewController: UIViewController {
+    @IBOutlet weak var textToTranslateTextView: UITextView!
+    @IBOutlet weak var textTranslatedTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    @IBAction func getTranslation(_ sender: UIButton) {
+        let textToTranslate = textToTranslateTextView.text
+        
+        if let text = textToTranslate {
+            TranslationService.shared.getTranslation(textToTranslate: text) { (success, translation) in
+                if success, let translation = translation {
+                    self.textTranslatedTextView.text! = translation.data.translations[0].translatedText
+                } else {
+                    self.showAlert(title: "Erreur!", message: "Requête invalide!")
+                }
+            }
+        } else {
+            showAlert(title: "Attention!", message: "Veuillez saisir du texte!")
+        }
+    }
+    
+    private func showAlert(title: String, message: String) {
+        let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+        self.present(alertVC, animated: true, completion: nil)
+    }
+
+}
+
+extension TranslationViewController: UITextViewDelegate {
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        textToTranslateTextView.resignFirstResponder()
+        textTranslatedTextView.resignFirstResponder()
+    }
+}
